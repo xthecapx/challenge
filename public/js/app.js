7194820190929c1a8f9b7ff63e85b89c642189d3
Cristian Casa <xthecapx@gmail.com>
2015-10-03 11:12:19 -0500

    Remove log in App

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,6 @@
 var App = (function() {
     return {
         Start: function (){
-            console.log('App Starting!');
             Router.init();
             Model.fetch('nav.json');
         }
